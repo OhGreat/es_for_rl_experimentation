398a977fb337efe297d22e7960bb5f365504256e
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-05-04 01:56:37 +0200

    to finish eval

---
+++ ESRL.py
@@ -0,0 +1,9 @@
+
+
+class ESRL():
+    def __init__(self, env, ep_max_len, budget):
+        self.env = env
+        self.ep_max_len = ep_max_len
+
+    def run():
+        pass
---
+++ test.py
@@ -0,0 +1,27 @@
+import os
+import gym
+import torch
+import argparse
+
+def main():
+    
+
+    # create gym environment
+    env = gym.make('CartPole-v1') # 'CartPole-v1'
+    envs = []
+    for i in range(100000):
+        envs.append(gym.make('CartPole-v1'))
+        envs[i].reset()
+    env_2 = gym.make('CartPole-v1')
+    if env is None:
+        exit("Please select an environment")
+    print(f"environment: {env.unwrapped.spec.id}")
+
+    n_actions = env.action_space.n
+    print(f"number of actions: {n_actions}")
+
+
+
+
+if __name__ == "__main__":
+    main()
